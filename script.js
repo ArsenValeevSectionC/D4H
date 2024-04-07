35ddce942ffef8255664e40d8f645bf98d1ca5fb
Gordon-Clarke-SectionC <n01303358@humber.ca>
2024-04-07 19:03:39 -0400

    Contact Page Update
    - Added Contact Form
    - Added Contact message
    - Fixed Nav bar

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ function toggleNav() {
     navMenu.classList.toggle("fadeIn"); 
   }
   
+  const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
+
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the form element
+  const form = document.querySelector(".contact-form");
+
+  // Reset the form fields upon page load
+  form.reset();
+});
+
+// Trigger the toast
+const toast = new bootstrap.Toast(document.getElementById('liveToast'));
+toast.show();
