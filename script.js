7c7697d8209aef1484e727e2bc65a2de19ebad24
Arsen_Valeev_SectionC <156844701+ArsenValeevSectionC@users.noreply.github.com>
2024-04-07 15:59:09 -0400

    Nav+Footer
    Created responsive navbar and footer

---
+++ script.js
@@ -0,0 +1,6 @@
+//navbar for mobile version change comment later !!!!!!!!!
+function toggleNav() {
+    var navMenu = document.getElementById("navMenu");
+    navMenu.classList.toggle("fadeIn"); 
+  }
+  
