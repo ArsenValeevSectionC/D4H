0336aba6f044fbd9bef2d2de9aaba5a665d63bd7
GabrielSosin-SectionC <n01646959@humber.ca>
2024-04-12 11:50:21 -0400

    Branching Gordon's changes
    UPdating Gordon's branch changes with my branch to make it easier for main merging

--- contactscript.js
+++ contactscript.js
@@ -1,28 +1,9 @@
 //navbar for mobile version change comment later !!!!!!!!!
 function toggleNav() {
-    var navMenu = document.getElementById("navMenu");
-    navMenu.classList.toggle("fadeIn"); 
-}
-  
-const toastTrigger = document.getElementById('liveToastBtn')
-const toastLiveExample = document.getElementById('liveToast')
-
-if (toastTrigger) {
-  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-  toastTrigger.addEventListener('click', () => {
-    toastBootstrap.show()
-  })
+  let navMenu = document.getElementById("navMenu");
+  navMenu.classList.toggle("active"); 
 }
 
-// Wait for the DOM to be fully loaded
-document.addEventListener("DOMContentLoaded", function () {
-  // Get the form element
-  const form = document.querySelector(".contact-form");
-
-  // Reset the form fields upon page load
-  form.reset();
-});
-
-// Trigger the toast
-const toast = new bootstrap.Toast(document.getElementById('liveToast'));
-toast.show();
+function clearResult(){
+  document.getElementById("result").value = ''
+}
